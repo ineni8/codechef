17e5da14e573209c06e9ae43ce717d9c264879e5
Ineni prasad <166210225+ineni8@users.noreply.github.com>
2024-08-15 23:28:48 +0530

    Add files via upload

---
+++ THREETOPICS.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c,d;
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	if(a==d)
+	{
+	    printf("YES");
+	}
+		else if(b==d)
+	{
+	    printf("YES");
+	}
+	   else if(c==d)
+	{
+	    printf("YES");
+	}
+	else printf("NO");
+
+}
+
